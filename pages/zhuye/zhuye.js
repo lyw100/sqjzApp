74bfe82747f8b1b2ff402fb48faef4b39665bdd5
lr <2512325727@qq.com>
2018-10-16 09:23:24 +0800

    laqu

--- pages/zhuye/zhuye.js
+++ pages/zhuye/zhuye.js
@@ -8,8 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
-
+     
   },
   shouyebof:function(){
     wx.navigateTo({
@@ -46,7 +45,7 @@ Page({
     })
 
     wx.request({
-      url: 'http://localhost:8081/SQJZ/sign/historySignList', //请求当月已选课程地址
+      url: 'http://localhost:8081/SQJZ/sign/historySignList', //请求历史已选课程地址
       data: {},
       header: {
         'content-type': 'application/json' // 默认值
@@ -59,6 +58,24 @@ Page({
         })
       }
     })
+
+    wx.request({
+      url: 'http://localhost:8081/SQJZ/sign/topCourseList', //获取点击量最多的3个课程
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data);
+        var list = res.data;
+        that.setData({
+          swiperCurrent:0,
+          imgUrls: list
+        })
+      }
+    })
+
+
 
   },
 
@@ -109,5 +126,15 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 点击滑动的图片
+   */
+  swipclick: function (e) {
+
   }
 })
+
+
+
