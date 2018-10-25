e18f37d30534f66d512665b296c804396d4f4e45
zc <2314155388@qq.com>
2018-10-25 17:16:05 +0800

    xinglipinggu

--- pages/ceshijieguo/ceshijieguo.js
+++ pages/ceshijieguo/ceshijieguo.js
@@ -4,14 +4,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that = this;
+    var psyrepid = options.psyrepid;
+    var jzid = getApp().globalData.jiaozhengid;
+    this.setData({
+      psyrepid: psyrepid,
+      jzid: jzid
+    })
+    wx.request({
+      url: getApp().globalData.url + '/psyass/getPsyReport', //获取90道心理评估试题
+      data: {
+        psyrepid: psyrepid,
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        var data = res.data;
+         that.setData({
+           bianhao: data.bianhao,
+           jieguo:data.jieguo,
+           jzname: data.jzname,
+           time: data.time,
+           zongfen: data.zongfen,
+           zongfenluehao: data.zongfenluehao,
+           zongfenmsg: data.zongfenmsg,
+           zongjunfen: data.zongjunfen,
+           zongjunfenluehao: data.zongjunfenluehao,
+           zongjunfenmsg: data.zongjunfenmsg,
+           yinzilist: data.yinzilist,
+         })
+      }
+    })
   },
 
   /**
