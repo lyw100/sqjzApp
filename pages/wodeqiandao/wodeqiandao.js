2ca359122226120fb78fbdfb294058ad8ccf3990
lr <2512325727@qq.com>
2018-12-18 17:47:56 +0800

    lr

--- pages/wodeqiandao/wodeqiandao.js
+++ pages/wodeqiandao/wodeqiandao.js
@@ -4,9 +4,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    yincangqdmk:true,
+    shualiandl:false,
     jrqdjc:false,
-    qdy_hui:true,
-    qdy_lan:false,
+    qdy_hui:false,
+    qdy_lan: true,
     tjym_hui: true,
     tjym_lan:false,
     qd_yemian:true,
@@ -36,6 +38,81 @@ Page({
       tjym_lan: true,
       qd_yemian: false,
       th_yemian: true,
+    })
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  myCatchTouch: function () {
+    // console.log('stop user scroll it!');
+    return;
+  },
+  /**
+  * 点击刷脸签到
+  */
+  shualianqiandao:function(){
+    this.setData({
+      yincangqdmk: false,
+      shualiandl: true,
+    })
+  },
+  /**
+  * 刷脸登录
+  */
+  takePhoto: function () {
+    var that = this;
+    clearTimeout(timer);//取消定时器
+    clearInterval(interval);//取消计时器
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        wx.showLoading({
+          title: '正在核验身份.....',
+        })
+        // this.setData({ logindisabled: true });
+        var header = getApp().globalData.header; //获取app.js中的请求头
+        wx.uploadFile({
+          url: getApp().globalData.url + '/course/face',
+          filePath: res.tempImagePath,
+          header: header,
+          formData: {
+            telephone: wx.getStorageSync("username")
+          },
+          name: 'file',
+          success: (res) => {
+            wx.hideLoading();
+            var data = JSON.parse(res.data);
+            if (data.msg == "OK") {
+              this.setData({
+                shualiandl: false,//是否展示刷脸窗口
+                xianshi: false,
+                face: true//验证通过
+              });
+              this.videoContext.play();//视频播放暂停
+
+            } else {
+              wx.showModal({
+                title: '提示',
+                content: data.msg,
+                showCancel: false,
+                success: function () {
+                  that.clearProgress();//重新加载定时器
+                }
+              })
+            }
+
+          }
+        })
+      }
+    })
+
+    this.setData({
+      yincangqdmk: true,
+      shualiandl: false,
     })
   },
   /**
